d0ec6ed27d60ee7d26668bacfd7f8796a4dfbc2e
Pablo <pablopessanh@gmail>
2020-10-12 22:28:36 -0300

    picking infos from API and showing titles

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from '../Tmdb';
+import MovieRow from '../MovieRow';
 
 export default () => {
-  return <div>Olá mundo!</div>;
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      const list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  }, []);
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
