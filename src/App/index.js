f347a793a2f359c50b2666b76e959499f486d4f9
Pablo <pablopessanh@gmail>
2020-10-12 23:42:43 -0300

    reorganize posters and titles; added mouse effects

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from '../Tmdb';
 import MovieRow from '../MovieRow';
+import './style.css';
+import FeaturedMovie from '../MovieRow/FeaturedMovie';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -13,8 +16,11 @@ export default () => {
 
     loadAll();
   }, []);
+
   return (
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
--- src/MovieRow/index.js
+++ src/MovieRow/index.js
@@ -3,8 +3,21 @@ import './style.css';
 
 export default ({ title, items }) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
+      <div className="list-area">
+        <div className="list">
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div className="list-item" key={key}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
